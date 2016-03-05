d684be11650893c269d675a4be08fe9b7e469eee
julien Eyriès <julien.eyries@jeyries.fr>
2016-03-05 22:13:25 +0100

    tools

--- iOS/Fishionary/AppDelegate.swift
+++ iOS/Fishionary/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        let database = DataManager.sharedInstance.database
+        print("count= \(database.count)")
+        
         // Override point for customization after application launch.
         let splitViewController = self.window!.rootViewController as! UISplitViewController
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController

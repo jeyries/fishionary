b4ab11bbf674d64319ceb5285903e726f1c16a32
Julien Eyries <julien@eyries.fr>
2019-01-24 12:56:53 +0100

    WIP

--- iOS/Fishionary/AppFlow.swift
+++ iOS/Fishionary/AppFlow.swift
@@ -102,27 +102,3 @@ final class AppFlow {
 
 
 
-private struct MainStoryboard {
-    
-    static var storyboard: UIStoryboard {
-        let bundle = Bundle.main
-        return UIStoryboard(name: "Main", bundle: bundle)
-    }
-    
-    static var masterViewController: MasterViewController {
-        return storyboard.instantiateViewController(withIdentifier: "master-scene") as! MasterViewController
-    }
-    
-    static var detailViewController: DetailViewController {
-        return storyboard.instantiateViewController(withIdentifier: "detail-scene") as! DetailViewController
-    }
-    
-    static var imageViewController: ImageViewController {
-        return storyboard.instantiateViewController(withIdentifier: "image-scene") as! ImageViewController
-    }
-    
-    static var settingsViewController: UIViewController {
-        return storyboard.instantiateViewController(withIdentifier: "settings-navigation")
-    }
-}
-
